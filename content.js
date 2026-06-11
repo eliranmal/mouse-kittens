1bd94b44e7cf9f95b179a6c15ad73a9b2964a2b5
lyn <lyn@redhat.com>
2026-06-11 07:41:24 +0300

    chore: defensive coding in content script data initialization

--- content.js
+++ content.js
@@ -1,38 +1,48 @@
 
-const setCatUrl = (name = 'cat-1') => {
+{
 
-  // same as referencing `__MSG_@@extension_id__` in CSS files
-  const extensionId = chrome.i18n.getMessage('@@extension_id');
-
-  // use fallback URLs, for chromium / gecko based browsers
-  const cursorUrls =
-      ['chrome-extension', 'moz-extension']
-          .map(protocol => `url('${protocol}://${extensionId}/images/${name}.png')`)
-          .join(', ');
-
-  document.documentElement.style.setProperty('--cursor-urls', cursorUrls);
-}
-
-const toggleCursor = () => {
-  document.documentElement.classList.toggle('cat-cursor');
-}
-
-chrome.runtime.onMessage.addListener((msg) => {
-  if (msg.command === 'toggle-cursor') {
-      toggleCursor();
+  const toggleCursor = () => {
+    document.documentElement.classList.toggle('cat-cursor');
   }
-});
 
-chrome.storage.local.onChanged.addListener(async (changes) => {
-  const currentData = await chrome.storage.local.get();
-  const catNameChange = changes['cat-name'];
-  setCatUrl(
-      catNameChange ? catNameChange.newValue : currentData['cat-name'],
-  );
-});
+  const setCatUrl = (name = 'cat-1') => {
 
-// populate initial values from storage
-chrome.storage.local.get().then((data) => (
+    // same as referencing `__MSG_@@extension_id__` in CSS files
+    const extensionId = chrome.i18n.getMessage('@@extension_id');
+
+    // use fallback URLs, for chromium / gecko based browsers
+    const cursorUrls =
+        ['chrome-extension', 'moz-extension']
+            .map(protocol => `url('${protocol}://${extensionId}/images/${name}.png')`)
+            .join(', ');
+
+    document.documentElement.style.setProperty('--cursor-urls', cursorUrls);
+  }
+
+
+  // register event listeners
+
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (msg.command === 'toggle-cursor') {
+        toggleCursor();
+    }
+  });
+
+  chrome.storage.local.onChanged.addListener(async (changes) => {
+    const currentData = await chrome.storage.local.get();
+    const catNameChange = changes['cat-name'];
+    setCatUrl(
+        catNameChange ? catNameChange.newValue : currentData['cat-name'],
+    );
+  });
+
+
+  // populate initial values from storage
+
+  chrome.storage.local.get({
+    'cat-name': 'cat-1',
+  }).then((data) => (
     setCatUrl(data['cat-name'])
-));
+  ));
 
+}
